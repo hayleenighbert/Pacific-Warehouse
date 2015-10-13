50e5f1a453573ec1498bbe007a429733406840c4
Haylee Nighbert <haylee.nigh@gmail.com>
2015-10-13 10:56:52 -0700

    adding possible choose your own adventure

--- js/gamescript.js
+++ js/gamescript.js
@@ -2,6 +2,89 @@ document.addEventListener('DOMContentLoaded', function() {
 	console.log('Voila!')
 });
 
+var current_page = 0;
+$(document).ready(function () {
+    load_page(0);
+    $('#response').on('click', '.choice', function () {
+        var target = $(this).data('target');
+        load_page(target);
+    });
+});
+
+function load_page(id) {
+    // Fetch JSON for page data associated with given ID
+    var page_data = PAGES[id];
+
+    clear_page();
+    set_page_text(page_data.text);
+    if (page_data.type === 'choice') {
+        for (var choice in page_data.choices) {
+            var choice_data = page_data.choices[choice];
+            add_choice(choice_data.text, choice_data.target);
+        }
+    }
+}
+
+function set_page_text(text) {
+    $("#page_text").append("<p>" + text + "</p>");
+}
+
+function add_choice(text, target) {
+    $("#response").append("<button class=choice data-target=" + target + ">" + text + "</button>");
+}
+
+function clear_page() {
+    $("#page_text").empty();
+    $("#response").empty();
+}
+
+// Page data
+var PAGES = [{
+    'text': 'You approach an abandoned warehouse. Which entrance do you take?',
+        'type': 'choice',
+        'choices': [{
+        'text': 'I am a traveler exploring the world.',
+            'target': 1
+    }, {
+        'text': 'I come from a farm on the outskirts of town.',
+            'target': 2
+    }, {
+        'text': 'I hail from a wealthy family. My father is a renowned merchant.',
+            'target': 3
+    }, {
+        'text': 'You don\'t need to know that.',
+            'target': 4
+    }]
+}, {
+    'text': 'Ah, a traveler? Fascinating!',
+        'type': 'choice',
+        'choices': [{
+        'text': 'Yes. Ask me that again.',
+            'target': 0
+    }]
+}, {
+    'text': 'Ah, the rural life on the farm?',
+        'type': 'choice',
+        'choices': [{
+        'text': 'Yes. Ask me that again.',
+            'target': 0
+    }]
+}, {
+    'text': 'A merchant\'s child? You\'re quite the young aristocrat, aren\'t you?',
+        'type': 'choice',
+        'choices': [{
+        'text': 'Yes. Ask me that again.',
+            'target': 0
+    }]
+}, {
+    'text': 'Ah, a shady type. Fine, you don\'t have to tell me.',
+        'type': 'choice',
+        'choices': [{
+        'text': 'Good. Ask me that again.',
+            'target': 0
+    }]
+}];
+
 
 //jquery select cursor
 // $( ".selector" ).draggable({
@@ -37,28 +120,27 @@ document.addEventListener('DOMContentLoaded', function() {
 
 //interactive bg 
 
-$(".bg").interactive_bg({
-   strength: 25,
-   scale: 1.05,
-   animationSpeed: "100ms",
-   contain: true,
-   wrapContent: false
+// $(".bg").interactive_bg({
+//    strength: 25,
+//    scale: 1.05,
+//    animationSpeed: "100ms",
+//    contain: true,
+//    wrapContent: false
 
-    });
-});
+// });
 
-$(document).ready(function(){
+// $(document).ready(function(){
 
-    $(".bg").interactive_bg(); // function call
-    });
+//     $(".bg").interactive_bg(); // function call
+//     });
 
-  // change background size on window resize
-  $(window).resize(function() {
-      $(".bg > .ibg-bg").css({
-        width: $(window).outerWidth(),
-        height: $(window).outerHeight()
-});
-  });
+//   // change background size on window resize
+//   $(window).resize(function() {
+//       $(".bg > .ibg-bg").css({
+//         width: $(window).outerWidth(),
+//         height: $(window).outerHeight()
+// });
+//   });
 
 
 
