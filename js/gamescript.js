d74b784b12637a5bbdcf2390c7f4f630475314b1
Haylee Nighbert <haylee.nigh@gmail.com>
2015-10-14 17:00:39 -0700

    Almost done! Fixed CSS and added more functionality. Need to add more story.

--- js/gamescript.js
+++ js/gamescript.js
@@ -9,26 +9,26 @@ $(document).ready(function(){
 
 
     $(".bg").interactive_bg(); // function call
+    $('.bg').hide().fadeIn('slow');
 
-    $('p').typewriter();
+
+    // $('p').typewriter({speed:50});
+    
+    $( "#draggable" ).draggable({ snap: true });
+    $( "#draggable2" ).draggable({ snap: ".ui-widget-header" });
+    $( "#draggable3" ).draggable({ snap: ".ui-widget-header", snapMode: "outer" });
+    $( "#draggable4" ).draggable({ grid: [ 20, 20 ] });
+    $( "#draggable5" ).draggable({ grid: [ 80, 80 ] });
+
 
 
 });
 
 
   // change background size on window resize
-// $(window).resize(function() {
-//     $(".bg > .ibg-bg").css({
-//         width: $(window).outerWidth(),
-//         height: $(window).outerHeight()
-//       })
-//     })
-
-// var GameObjects = new Array('', "notepad", "camera", "key", "graffiti");
-// $(".bg").interactive_bg({
-//     strength: 25,              // Movement Strength when the cursor is moved. The higher, the faster it will reacts to your cursor. The default value is 25.
-//     scale: 1.05,               // The scale in which the background will be zoomed when hovering. Change this to 1 to stop scaling. The default value is 1.05.
-//     animationSpeed: "100ms",   // The time it takes for the scale to animate. This accepts CSS3 time function such as "100ms", "2.5s", etc. The default value is "100ms".
-//     contain: true,             // This option will prevent the scaled object/background from spilling out of its container. Keep this true for interactive background. Set it to false if you want to make an interactive object instead of a background. The default value is true.
-//     wrapContent: false         // This option let you choose whether you want everything inside to reacts to your cursor, or just the background. Toggle it to true to have every elements inside reacts the same way. The default value is false
-//  });
+$(window).resize(function() {
+    $(".bg > .ibg-bg").css({
+        width: $(window).outerWidth(),
+        height: $(window).outerHeight()
+      })
+    })
