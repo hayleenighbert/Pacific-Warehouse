ad2f5066d7a0ef6a335f2a50f0c2485c3e999a84
Haylee Nighbert <haylee.nigh@gmail.com>
2015-10-14 22:41:15 -0700

    Adding function that allows you to win after three items are gathered/added more slides

--- js/gamescript.js
+++ js/gamescript.js
@@ -2,27 +2,31 @@
 // 	console.log('Voila!')
 // });
 
-$(document).ready(function(){
+
+//Working on winning option - collect three items and you win!
+
+// var count = 0;
+// $('.button').on('click', function () {
+//     // do things to add item to inventory.
+//     count++
+//     if (count == 3) {
+//         alert('You win!')
+//     });
+
+// $(document).ready(function(){
+//     $(".button").click(function(){
+//     var color = $( this ).css( "hidden" );
+//     });
+
+//     $('#ci').chooseIt();;
 
 
-    $('#ci').chooseIt();;
-
-
-    $(".bg").interactive_bg(); // function call
-    $('.bg').hide().fadeIn('slow');
-
-
-    // $('p').typewriter({speed:50});
-    
-    $( "#draggable" ).draggable({ snap: true });
-    $( "#draggable2" ).draggable({ snap: ".ui-widget-header" });
-    $( "#draggable3" ).draggable({ snap: ".ui-widget-header", snapMode: "outer" });
-    $( "#draggable4" ).draggable({ grid: [ 20, 20 ] });
-    $( "#draggable5" ).draggable({ grid: [ 80, 80 ] });
+//     $(".bg").interactive_bg(); // function call
+//     $('.bg').hide().fadeIn('slow');
 
 
 
-});
+// });
 
 
   // change background size on window resize
@@ -32,3 +36,15 @@ $(window).resize(function() {
         height: $(window).outerHeight()
       })
     })
+
+
+
+
+
+// Future draggable option for inventory    
+    // $( "#draggable" ).draggable({ snap: true });
+    // $( "#draggable2" ).draggable({ snap: ".ui-widget-header" });
+    // $( "#draggable3" ).draggable({ snap: ".ui-widget-header", snapMode: "outer" });
+    // $( "#draggable4" ).draggable({ grid: [ 20, 20 ] });
+    // $( "#draggable5" ).draggable({ grid: [ 80, 80 ] });
+
