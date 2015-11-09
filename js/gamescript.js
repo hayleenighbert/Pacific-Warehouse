8703a911978ac578d7cde65d5086dfafad87fbaf
Haylee Nighbert <haylee.nigh@gmail.com>
2015-11-09 14:15:18 -0800

    Updated README

--- js/gamescript.js
+++ js/gamescript.js
@@ -11,13 +11,49 @@ $('.item').on('click', function () {
         alert('You win! You have collected all the necessary clues to find out more about your warehouse adventure. Continue on to the next slide for your fate..');
     }
 
+});
 
-//hide button after click
-// $('#button').click(function() {
-//     $(this).hide();
-// });
+//make button disappear on click depending on class
+$('#button').click(function() {
+    $(this).hide();
+    // console.log('disappearing button');
 
 });
+
+
+//The magic code to add show/hide custom event triggers
+// (function ($) {
+//       $.each(['show', 'hide'], function (i, ev) {
+//         var el = $.fn[ev];
+//         $.fn[ev] = function () {
+//           this.trigger(ev);
+//           return el.apply(this, arguments);
+//         };
+//       });
+//     })(jQuery);
+ 
+ 
+ 
+// //on Show button click; show the #foo div
+// $('#btnShow').click(function(){
+//    $('#foo').show();
+// });
+ 
+// //on Hide button click; hide the #foo div
+// $('#btnHide').click(function(){
+//    $('#foo').hide();
+// });
+ 
+// //Add custom handler on show event and print message
+// $('#foo').on('show', function(){
+//     $('#console').html( $('#console').html() + '#foo is now visible'+ '<br>'  )
+// });
+ 
+// //Add custom handler on hide event and print message
+// $('#foo').on('hide', function(){
+//     $('#console').html( $('#console').html() + '#foo is hidden'+ '<br>'  )
+// });
+
 
     $('#ci').chooseIt();
     $(".bg").interactive_bg(); // function call
