dff427119689425ab4d69cfc88acca182d944e82
Haylee Nighbert <haylee.nigh@gmail.com>
2016-01-04 14:33:08 -0800

    Pacific Warehouse

---
+++ images/Haylee Nighbert - Web Developer_files/index.js
@@ -0,0 +1,23 @@
+ $(document).ready(function(){
+      $('.parallax').parallax();
+    });
+
+
+
+// (function(){
+
+//   var parallax = document.querySelectorAll(".parallax"),
+//       speed = 0.5;
+
+//   window.onscroll = function(){
+//     [].slice.call(parallax).forEach(function(el,i){
+
+//       var windowYOffset = window.pageYOffset,
+//           elBackgrounPos = "50% " + (windowYOffset * speed) + "px";
+
+//       el.style.backgroundPosition = elBackgrounPos;
+
+//     });
+//   };
+
+// })();
